cb151302f57ea1693bafc0e2db37ff28eb7b3415
HumbleCatcher <andreas.ellison@gmail.com>
2023-02-01 23:55:42 +0100

    fix cursor bug and refactor + format code

--- ace-enable-vim.js
+++ ace-enable-vim.js
@@ -1,87 +1,113 @@
 const w = window.wrappedJSObject;
-const app = document.getElementById("app");
-let path = window.location.pathname;
+let currentPath = window.location.pathname;
 let observerAttached = false;
 let scriptLoaded = false;
 let extensionEnabled = true;
 
-function tryEnable() {
-    if (app.getElementsByClassName("ace_editor").length > 0) {
-        onload = () => {
-            for (let n of app.getElementsByClassName("ace_editor")) {
-                e = n.wrappedJSObject.env.editor;
-                e.setKeyboardHandler(w.ace.require("ace/keyboard/vim").handler);
-            }
-        };
-        if (!scriptLoaded) {
-            let script = document.createElement("script");
-            script.src = browser.runtime.getURL("ace-keybinding-vim.js");
-            script.onload = onload;
-            document.head.appendChild(script);
-            scriptLoaded = true;
-        } else {
-            onload();
-        }
-        return true;
-    }
-    return false;
+function pageHasAceEditor() {
+  return document.getElementsByClassName("ace_editor").length > 0;
 }
 
-const observer = new MutationObserver((_, observer) => {
-    if (tryEnable()) {
-        observer.disconnect();
-        observerAttached = false;
+function enableVimMode() {
+  onload = () => {
+    for (let n of document.getElementsByClassName("ace_editor")) {
+      e = n.wrappedJSObject.env.editor;
+      e.setKeyboardHandler(w.ace.require("ace/keyboard/vim").handler);
+      // solves a bug where
+      if (!n.classList.contains("normal-mode")) {
+        n.classList.add("normal-mode");
+      }
     }
+  };
+  // avoid adding the script to the document head multiple times
+  if (!scriptLoaded) {
+    let script = document.createElement("script");
+    script.src = browser.runtime.getURL("ace-keybinding-vim.js");
+    script.onload = onload;
+    document.head.appendChild(script);
+    scriptLoaded = true;
+  } else {
+    onload();
+  }
+}
+
+function enable() {
+  if (pageHasAceEditor()) {
+    enableVimMode();
+  } else {
+    attachObserver();
+  }
+}
+
+function disableVimMode() {
+  for (let n of document.getElementsByClassName("ace_editor")) {
+    e = n.wrappedJSObject.env.editor;
+    e.setKeyboardHandler("");
+  }
+}
+
+const observer = new MutationObserver(() => {
+  if (pageHasAceEditor()) {
+    enableVimMode();
+    detachObserver();
+  }
 });
 
 function attachObserver() {
-    if (!path.startsWith("/ide2")) {
-        return;
-    }
-    observer.observe(app, {
-        childList: true,
-        subtree: true,
+  const pathPrefixesToCXEditor = ["ide2", "solve"];
+  if (
+    pathPrefixesToCXEditor.some((prefix) =>
+      currentPath.startsWith(`/${prefix}`)
+    )
+  ) {
+    observer.observe(document, {
+      childList: true,
+      subtree: true,
     });
     observerAttached = true;
+  }
+}
+
+function detachObserver() {
+  observer.disconnect();
+  observerAttached = false;
 }
 
 browser.storage.local.get({ enabled: true }).then((results) => {
-    extensionEnabled = results.enabled;
-    if (extensionEnabled) {
-        if (!tryEnable()) {
-            attachObserver();
-        }
-    }
+  extensionEnabled = results.enabled;
+  if (extensionEnabled) {
+    enable();
+  }
 });
 
+/**
+ * Navigations occur without a proper page load. Thus this script will not get reloaded on navigations and if
+ * the first load of the page is not the editor, we need to wait for an editor to appear to enable Vim mode.
+ * To avoid having the Mutation Observer constantly active, we try to detect navigations, enable the Mutation
+ * Observer if on a page that will have an editor and immediately detach the observer once Vim mode has been enabled.
+ */
 function onNavigation() {
-    if (!extensionEnabled) return;
-    let currentPath = window.location.pathname;
-    if (!observerAttached && path !== currentPath) {
-        path = currentPath;
-        attachObserver();
-    }
+  if (!extensionEnabled) return;
+  let newPath = window.location.pathname;
+  if (!observerAttached && currentPath !== newPath) {
+    currentPath = newPath;
+    enable();
+  }
 }
 
 window.addEventListener("popstate", onNavigation);
 window.addEventListener("click", onNavigation);
 
 browser.storage.local.onChanged.addListener((changes) => {
-    extensionEnabled = changes.enabled.newValue;
-    if (extensionEnabled) {
-        if (!tryEnable()) {
-            attachObserver();
-        }
-    } else {
-        if (observerAttached) {
-            observer.disconnect();
-            observerAttached = false;
-        }
-        // set keybindings back to the default
-        for (let n of app.getElementsByClassName("ace_editor")) {
-            e = n.wrappedJSObject.env.editor;
-            e.setKeyboardHandler("");
-        }
-        path = "";
+  extensionEnabled = changes.enabled.newValue;
+  if (extensionEnabled) {
+    enable();
+    // keep track of current path again (might have changed since add-on was disabled)
+    currentPath = window.location.pathname;
+  } else {
+    if (observerAttached) {
+      detachObserver();
     }
+    disableVimMode();
+  }
 });
