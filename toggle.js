acab0c6fcda78ac309acdb9f5164fe3b58ad10b5
HumbleCatcher <andreas.ellison@gmail.com>
2023-02-19 18:28:31 +0100

    migrate `toggle.js` to work as service worker

--- toggle.js
+++ toggle.js
@@ -1,12 +1,21 @@
+function setIcon(enabled) {
+  if (enabled) {
+    browser.browserAction.setIcon({ path: "icon.png" });
+  } else {
+    browser.browserAction.setIcon({ path: "icon-grayscale.png" });
+  }
+}
+
+// initialize icon to match current value of `enabled`
+browser.storage.local.get({ enabled: true }).then((results) => {
+  setIcon(results.enabled);
+});
+
 // toggle extension when icon is clicked
 browser.browserAction.onClicked.addListener(() => {
-    browser.storage.local.get({ enabled: true }).then((results) => {
-        let enabled = !results.enabled;
-        if (enabled) {
-            browser.browserAction.setIcon({ path: "icon.png" });
-        } else {
-            browser.browserAction.setIcon({ path: "icon-grayscale.png" });
-        }
-        browser.storage.local.set({ enabled });
-    });
+  browser.storage.local.get({ enabled: true }).then((results) => {
+    let enabled = !results.enabled;
+    setIcon(enabled);
+    browser.storage.local.set({ enabled });
+  });
 });
