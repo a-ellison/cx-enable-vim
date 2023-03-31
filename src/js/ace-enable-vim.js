5887687ed712a9c617168db33ff0792a614c109c
HumbleCatcher <andreas.ellison@gmail.com>
2023-03-31 22:51:37 +0200

    use new 'cxAceMounted' event emitted by Code Expert to replace MutationObserver logic

--- src/js/ace-enable-vim.js
+++ src/js/ace-enable-vim.js
@@ -1,5 +1,3 @@
-let currentPath = window.location.pathname;
-let observerAttached = false;
 let scriptLoaded = false;
 let extensionEnabled = true;
 
@@ -39,36 +37,7 @@ function pageHasAceEditor() {
 function enable() {
   if (pageHasAceEditor()) {
     enableVimMode();
-  } else {
-    attachObserver();
   }
-}
-
-const observer = new MutationObserver(() => {
-  if (pageHasAceEditor()) {
-    enableVimMode();
-    detachObserver();
-  }
-});
-
-function attachObserver() {
-  const pathPrefixesToCXEditor = ["ide2", "solve"];
-  if (
-    pathPrefixesToCXEditor.some((prefix) =>
-      currentPath.startsWith(`/${prefix}`)
-    )
-  ) {
-    observer.observe(document, {
-      childList: true,
-      subtree: true,
-    });
-    observerAttached = true;
-  }
-}
-
-function detachObserver() {
-  observer.disconnect();
-  observerAttached = false;
 }
 
 browser.storage.local.get({ enabled: true }).then((results) => {
@@ -79,33 +48,23 @@ browser.storage.local.get({ enabled: true }).then((results) => {
 });
 
 /**
- * Navigations occur without a proper page load. Thus this script will not get reloaded on navigations and if
- * the first load of the page is not the editor, we need to wait for an editor to appear to enable Vim mode.
- * To avoid having the Mutation Observer constantly active, we try to detect navigations, enable the Mutation
- * Observer if on a page that will have an editor and immediately detach the observer once Vim mode has been enabled.
+ * The "cxAceMounted" event is explicitly emitted by Code Expert to notify this extension.
  */
-function onNavigation() {
-  if (!extensionEnabled) return;
-  let newPath = window.location.pathname;
-  if (!observerAttached && currentPath !== newPath) {
-    currentPath = newPath;
-    enable();
-  }
-}
-
-window.addEventListener("popstate", onNavigation);
-window.addEventListener("click", onNavigation);
+document.addEventListener(
+  "cxAceMounted",
+  () => {
+    if (extensionEnabled) {
+      enable();
+    }
+  },
+  { capture: true }
+);
 
 browser.storage.local.onChanged.addListener((changes) => {
   extensionEnabled = changes.enabled.newValue;
   if (extensionEnabled) {
     enable();
-    // keep track of current path again (might have changed since extension was disabled)
-    currentPath = window.location.pathname;
   } else {
-    if (observerAttached) {
-      detachObserver();
-    }
     reinjectScript("js/set-default-keyboard-handler.js");
   }
 });
